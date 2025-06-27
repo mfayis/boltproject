cbed599f1ca8b2da28a9e3b5d6809ebc25744a16
MUHAMMED FAYIS BIN FAISAL <116888738+mfayis@users.noreply.github.com>
2025-06-28 03:11:01 +0530

    Updated ResultCard.tsx

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -93,7 +93,6 @@ export default function ResultCard({ result }: ResultCardProps) {
                 <Text style={styles.xpText}>{winner.xp}</Text>
               </View>
             </View>
-            
           </View>
         ))}
       </View>
