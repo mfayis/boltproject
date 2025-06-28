35aacba7045c1469a78e05a4dec55b2f63bd08b0
MUHAMMED FAYIS BIN FAISAL <116888738+mfayis@users.noreply.github.com>
2025-06-28 12:19:53 +0530

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -174,7 +174,7 @@ export default function ChallengesPage() {
                       styles.joinButtonText,
                       challenge.completed && styles.completedButtonText
                     ]}>
-                      {challenge.completed ? 'Completed! 🎉' : 'Join'}
+                      {challenge.completed ? 'Completed! 🎉' : 'Join 🤠'}
                     </Text>
                   </TouchableOpacity>
                 </View>
