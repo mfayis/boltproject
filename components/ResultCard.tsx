965d84dcde4f57f60a6dda5de4c9ec49d49d1ede
MUHAMMED FAYIS BIN FAISAL <116888738+mfayis@users.noreply.github.com>
2025-06-28 12:10:00 +0530

    Updated ResultCard.tsx

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -236,7 +236,7 @@ const styles = StyleSheet.create({
   },
   certificateButton: {
     backgroundColor: '#00D4AA',
-    paddingHorizontal: 16,
+    paddingHorizontal: 14,
     paddingVertical: 8,
     borderRadius: 12,
   },
