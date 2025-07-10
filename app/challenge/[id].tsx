cb68036a42845b1b1002844b3f5bd5376807a123
MUHAMMED FAYIS BIN FAISAL <116888738+mfayis@users.noreply.github.com>
2025-07-10 12:11:10 +0530

    Updated [id].tsx

--- app/challenge/[id].tsx
+++ app/challenge/[id].tsx
@@ -183,7 +183,7 @@ const styles = StyleSheet.create({
     color: '#FFD700',
   },
   statValue: {
-    fontSize: 2,
+    fontSize: 15,
     fontWeight: 'bold',
     color: '#00FFD1',
     marginBottom: 2,
