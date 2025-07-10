3c812caed73dea41bd70692f4b46c59e1e7fdbef
MUHAMMED FAYIS BIN FAISAL <116888738+mfayis@users.noreply.github.com>
2025-07-10 13:21:08 +0530

    Updated [id].tsx

--- app/challenge/[id].tsx
+++ app/challenge/[id].tsx
@@ -163,6 +163,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 2,
     borderColor: '#2D254D',
+    alignItems: 'center',
     marginHorizontal: 4,
     paddingVertical: 20,
     paddingHorizontal: 0,
